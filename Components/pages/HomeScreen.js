71ff2efe3d94ddf4d4dd599a1b92a69674e5145b
Jonathanghl <31654288+Jonathanghl@users.noreply.github.com>
2018-11-15 12:05:02 +0100

    FJernet udkommentering

--- Components/pages/HomeScreen.js
+++ Components/pages/HomeScreen.js
@@ -19,7 +19,6 @@ export default class HomeScreen extends React.Component {
   };
 
   componentDidMount() {
-   /* this.getAlbumsFromApiAsync();*/
     this.getBoardGamesFromApiAsync();
   }
 
@@ -31,63 +30,15 @@ export default class HomeScreen extends React.Component {
       .ref("BoardGames")
       .on("value", function(snapshot) {
         var boardGames = Object.values(snapshot.val());
-        //Brug artist ID til at hente fulde navn og erstat dataen.
-        //Da dataen i øvelserne kun er fra Taylor Swift, går vi bare ind i første object i Arrayet,
-        //da vi ved alle objekter har samme artist. Er der forskellige, kan man loope igennem arrayet og erstatte variabler
-        //var artistID = albums[0].artist;
-
-        /*Lav et nyt database-kald:
-        firebase
-          .database()
-          .ref("artists/" + artistID)
-          .once("value", function(snapshotArtist) {
-            //loop over albums og erstat
-            albums.forEach(function(album) {
-              album.artist =
-                snapshotArtist.val().firstName +
-                " " +
-                snapshotArtist.val().lastName;
-            });*/
+        
             that.setState({
               isLoading: false,
               dataSource: boardGames 
             });
           });
-      /*});*/
   }
 
- /* getAlbumsFromApiAsync() {
-    var that = this;
-
-    return firebase
-      .database()
-      .ref("Albums")
-      .on("value", function(snapshot) {
-        var albums = Object.values(snapshot.val());
-        //Brug artist ID til at hente fulde navn og erstat dataen.
-        //Da dataen i øvelserne kun er fra Taylor Swift, går vi bare ind i første object i Arrayet,
-        //da vi ved alle objekter har samme artist. Er der forskellige, kan man loope igennem arrayet og erstatte variabler
-        var artistID = albums[0].artist;
-
-        //Lav et nyt database-kald:
-        firebase
-          .database()
-          .ref("artists/" + artistID)
-          .once("value", function(snapshotArtist) {
-            //loop over albums og erstat
-            albums.forEach(function(album) {
-              album.artist =
-                snapshotArtist.val().firstName +
-                " " +
-                snapshotArtist.val().lastName;
-            });
-            that.setState({
-              isLoading: false,
-              dataSource: albums
-            });
-          });
-      });
-  }*/
+ 
 
   render() {
     if (this.state.isLoading) {

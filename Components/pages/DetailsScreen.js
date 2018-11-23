1b56ab8faf1dcca8f886102a44b5b6101aa2ecef
male16ak <male16ak@student.cbs.dk>
2018-11-23 10:47:23 +0100

    Added styling to Details Screen

--- Components/pages/DetailsScreen.js
+++ Components/pages/DetailsScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Image, Alert, View, StyleSheet, Text } from "react-native";
-import { Button } from "react-native-elements";
+import { Button, Card } from "react-native-elements";
 import Icon from "react-native-vector-icons/FontAwesome";
 import firebase from "firebase";
 
@@ -26,24 +26,47 @@ export default class DetailsScreen extends React.Component {
     );
     const image = navigation.getParam("image", "Intet billede defineret");
     const antal = navigation.getParam("antal", "Intet specificerede antal");
+    const ejer = navigation.getParam("ejer", "Ingen specificerede ejer");
 
     return (
       <View>
+        <Card title={title}>
+          <Image
+            style={{ width: 310, height: 300, marginBottom: 15 }}
+            source={{ uri: image }}
+          />
+          <Text style={{ marginBottom: 10 }}> Genre: {genre}</Text>
+          <Text style={{ marginBottom: 10 }}>
+            {" "}
+            Anbefalet alder: {aldersgruppe}{" "}
+          </Text>
+          <Text style={{ marginBottom: 10 }}> Antal spillere: {antal}</Text>
+          <Text style={{ marginBottom: 10 }}> Ejer: {ejer}</Text>
 
+          <Button
+            icon={{ name: "code" }}
+            backgroundColor="#03A9F4"
+            buttonStyle={{
+              borderRadius: 0,
+              marginLeft: 0,
+              marginRight: 0,
+              marginBottom: 0
+            }}
+            title="Lej Spil"
+          />
+        </Card>
         <Image style={{ width: 105, height: 135 }} source={{ uri: image }} />
-
 
         <Text>Titel: {title}</Text>
         <Text>Genre: {genre}</Text>
         <Text>Anbefalet alder: {aldersgruppe} </Text>
         <Text>Antal spillere: {antal}</Text>
-
+        <Text>Ejer: {ejer}</Text>
 
         <Button
           icon={<Icon name="arrow-right" size={15} color="white" />}
           title="lej spil"
           onPress={this.lejSpil}
-
         />
       </View>
     );
